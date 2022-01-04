21a88fef796f69b61ab894efdfd25ee4b99298ce
amyzhang <amyyzz08@gmail.com>
2022-01-04 14:32:46 +0800

    in progress

--- code/static/js/app.js
+++ code/static/js/app.js
@@ -1,37 +1,34 @@
+function dropdown(selectedID) {
 
     // Use D3 Library to fetch the JSON data
     d3.json("samples.json").then((data) => {
     
-    // console.log(data);
-
-    // clear dropdown menu
-    d3.select("#selDataset").html("");
-
-    // Iterating through array to select each individual ID and add to dropdown menu
-    data.metadata.forEach(row => {
-
-        var id = row.id
-        // console.log(id);
-        
-        // Appending all individual IDs to the dropdown menu
-        d3.select("#selDataset").append('option').attr('value',id).text(id);    
-
-        var selectedID = d3.select("#selDataset").node().value;
-
-        const arrayID = data.metadata.filter(row => (row.id == selectedID));
-        console.log(arrayID)
+        // console.log(data);
     
-         // Appending info to panel
-        var panel = d3.select("#sample-metadata");
-        panel.html("");
-        Objects.entries(arrayID[0]).forEach(row=>{
-            panel.append("option").text(`${row[0]}: ${row[1]}`)
+        // clear dropdown menu
+        d3.select("#selDataset").html("");
+    
+        // Iterating through array to select each individual ID and add to dropdown menu
+        data.metadata.forEach(row => {
+    
+            var id = row.id
+            // console.log(id);
+            
+            // Appending all individual IDs to the dropdown menu
+            d3.select("#selDataset").append('option').attr('value',id).text(id);  
+          
+    
+            d3.select("#selDataset").node().value = selectedID;
+    
+            const arrayID = data.metadata.filter(row => (row.id == selectedID));
+            console.log(arrayID)
+        
+            // Appending demographic info
+            var panel = d3.select("#sample-metadata").html("");
+            
+            Objects.entries(arrayID[0]).forEach(([key,value]) =>{
+                panel.append("option").text(`${key}: ${value}`)
+            });      
         });
-
     });
-
-
-
-
-
-})
+}
