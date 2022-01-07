946c9986b1916c48ea15ab20419ba24e0542dc91
amyzhang <amyyzz08@gmail.com>
2022-01-07 12:02:41 +0800

    bar chart code

--- code/static/js/app.js
+++ code/static/js/app.js
@@ -1,34 +1,66 @@
+// Creating an event listener
+d3.select("#selDataset").on("change",function(){
+    dropdown(d3.select("#selDataset").node().value)
+})
+
 function dropdown(selectedID) {
 
     // Use D3 Library to fetch the JSON data
     d3.json("samples.json").then((data) => {
     
         // console.log(data);
-    
-        // clear dropdown menu
-        d3.select("#selDataset").html("");
-    
-        // Iterating through array to select each individual ID and add to dropdown menu
-        data.metadata.forEach(row => {
-    
-            var id = row.id
-            // console.log(id);
-            
-            // Appending all individual IDs to the dropdown menu
-            d3.select("#selDataset").append('option').attr('value',id).text(id);  
           
+        d3.select("#selDataset").node().value = selectedID;
     
-            d3.select("#selDataset").node().value = selectedID;
-    
-            const arrayID = data.metadata.filter(row => (row.id == selectedID));
-            console.log(arrayID)
+        const arrayID = data.metadata.filter(row => (row.id == selectedID));
+        // console.log(arrayID)
         
-            // Appending demographic info
-            var panel = d3.select("#sample-metadata").html("");
+        // Appending demographic info to panel
+        var panel = d3.select("#sample-metadata").html("");
             
-            Objects.entries(arrayID[0]).forEach(([key,value]) =>{
-                panel.append("option").text(`${key}: ${value}`)
-            });      
-        });
+        Object.entries(arrayID[0]).forEach(([key,value]) =>{
+            panel.append("option").text(`${key}: ${value}`)
+        });      
     });
 }
+
+// Appending all the individual ID to dropdown
+d3.json("samples.json").then((data) => {
+    data.metadata.forEach(row => {
+        d3.select("#selDataset").append('option').attr('value',row.id).text(row.id);
+    })  
+});
+
+
+// Build Horizontal Bar and Bubble charts
+function charts(selectedID) {
+    d3.json("samples.json").then((data) => {
+
+        var input = data.samples.filter(row=> row.id == selectedID);
+        var result = input[0]
+
+        var sampleValue = result.sample_values;
+        var otuID = result.otu_ids;
+        var labels = result.otu_labels;
+
+        var bar_trace = {
+            x:sampleValue.slice(0,10).reverse(),
+            y:otuID.slice(0,10).map(i => `OTU ${i}`).reverse(),
+            text:labels.slice(0,10).reverse(),
+            type: "bar",
+            orientation: "h"
+        };
+
+        var bar_data = [bar_trace];
+
+        var bar_layout = {
+            title: "Top 10 OTU Found" 
+        };
+    
+        Plotly.newPlot("bar",bar_data,bar_layout);
+
+    });
+   
+}
+
+
